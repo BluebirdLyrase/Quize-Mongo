e168b5f5982fd3cd1a5dbeecd8392e39f536e8e3
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-09-19 12:09:23 +0700

    YAYYYYYY

--- server.js
+++ server.js
@@ -3,20 +3,62 @@
 var express = require('express');
 var app = express();
 var data = require("./data.json")
+// set up Mongo
+var mongo = require("mongodb");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var options = { useUnifiedTopology: true, useNewUrlParser: true };
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+
 
 // use res.render to load up an ejs view file
 
 // index page 
-app.get('/', function(req, res) {
-    res.render('pages/Home',{thisdata: data});
+app.get('/', function (req, res) {
+    res.render('pages/Home', { thisdata: data });
 });
 
 // about page 
-app.get('/Student', function(req, res) {
-    res.render('pages/Student',{thisdata: data});
+app.get('/Student', function (req, res) {
+    res.render('pages/Student', { thisdata: data });
+});
+
+app.get('/class', function (req, res) {
+    MongoClient.connect(url, options, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("coc");
+        var query = {};
+        dbo.collection("classroom")
+            .find(query)
+            .toArray(function (err, result) {
+                if (err) throw err;
+                // console.log(result);
+                res.render('pages/class', { classes: result });
+                db.close();
+            });
+    });
+});
+
+app.get('/classdetail/:id', function (req, res) {
+    var classid = req.params.id;
+    MongoClient.connect(url, options, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("coc");
+        var query = {subject_id:classid};
+        dbo.collection("classroom")
+            .findOne(query,function (err, result) {
+                if (err) throw err;
+                console.log(result);
+                console.log(classid);
+                res.render('pages/classdetail', { detail: result });
+                db.close();
+            });
+    });
 });
 
 app.listen(8080);
 console.log('MAGIC MAGIC MAGIC MAGIC MAGIC');
+
